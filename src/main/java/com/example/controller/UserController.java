6d6e749ee5a941ef4fbf884e48e4e06cdcd6eaab
yanyu <2273153713@qq.com>
2023-03-11 02:48:17 +0800

    阿里云短信服务功能开发

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,92 @@
+package com.example.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.example.common.R;
+import com.example.entity.User;
+import com.example.service.UserService;
+import com.example.utils.SMSUtils;
+import com.example.utils.ValidateCodeUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    /**
+     * 发送手机短信验证码
+     * @author yanyu
+     * @datetime 2023/3/11 1:47
+     * @param user
+     * @return
+     */
+    @PostMapping("/sendMsg")
+    public R<String> sendMsg(@RequestBody User user, HttpSession session){
+        //获取手机号
+        String phone = user.getPhone();
+        if (StringUtils.isNotEmpty(phone)){
+            //生成随机的4位验证码
+            String code = ValidateCodeUtils.generateValidateCode(4).toString();
+            log.info("打印验证码code：{}",code);
+            //调用阿里云提供的短信服务API完成发送短信
+//            SMSUtils.sendMessage();
+
+            //需要将生成的验证码保存到Session
+            session.setAttribute(phone,code);
+
+            return R.success("手机验证码短信发送成功(#^.^#)");
+        }
+
+        return R.error("短信发送失败┭┮﹏┭┮");
+    }
+
+    /**
+     * 移动端用户登录
+     * @author yanyu
+     * @datetime 2023/3/11 2:26
+     * @param user
+     * @param session
+     * @return
+     */
+    @PostMapping("/login")
+    public R<User> login(@RequestBody Map map, HttpSession session){
+      log.info(map.toString());
+        //获取手机号
+        String phone = map.get("phone").toString();
+        //获取验证码
+        String code = map.get("code").toString();
+
+        //从session中获取保存的验证码
+        Object codeInSession = session.getAttribute(phone);
+
+        //进行验证码的比对（页面提交的验证码和session中保存的验证码比对）
+        if (codeInSession != null && codeInSession.equals(code)){
+            //如果能够比对成功，说明登录成功
+            LambdaQueryWrapper<User> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+            lambdaQueryWrapper.eq(User::getPhone,phone);
+
+            User user = userService.getOne(lambdaQueryWrapper);
+            if (user == null){
+                //判断当前手机号对应的用户身份为新用户，如果是新用户就自动完成注册
+                user = new User();
+                user.setPhone(phone);
+                user.setStatus(1);
+                userService.save(user);
+            }
+            R.success(user);
+        }
+
+        return R.error("登录失败┭┮﹏┭┮");
+    }
+}
